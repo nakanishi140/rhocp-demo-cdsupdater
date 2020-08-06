32d52773037f1b1d59c97a2d2f1a7852c5c073eb
yuu <fj7267hc@aa.jp.fujitsu.com>
2020-08-06 15:49:49 +0900

    ddd

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ -d /demo/rhocp-demo-contents ]; then
+if [ -d /demo/contents ]; then
 
-cd /demo/rhocp-demo-contents
+cd /demo/contents
 git pull
 
 else 
 
-git clone https://github.com/yuu-ymt/rhocp-demo-contents.git /demo
+git clone https://github.com/yuu-ymt/rhocp-demo-contents.git /demo/contents
 
 fi
 
